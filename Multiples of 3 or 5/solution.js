f980cd1a3316c4e9c851b980e65af85a2f090704
f-mohrechi <f.mohrechi78@gmail.com>
2023-12-29 17:28:04 +0330

    sum of multiples of 3 or 5

---
+++ Multiples of 3 or 5/solution.js
@@ -0,0 +1,12 @@
+function Sum(number) {
+  let sum = 0;
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+console.log(Sum(20));
