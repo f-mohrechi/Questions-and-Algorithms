092107e516593534f94ba768c1a61f46d695e39a
m-mohrechi <mahi@tekup.org>
2024-05-07 18:26:26 +0330

    max of two numbers

---
+++ Max of Two Numbers/question.md
@@ -0,0 +1,1 @@
+## write a function that takes two numbers and returns the maximum of the two.
---
+++ Max of Two Numbers/solution.js
@@ -0,0 +1,5 @@
+const maxNum = (num1, num2) => {
+  return num1 > num2 ? num1 : num2;
+};
+
+console.log(maxNum(21, 22));
